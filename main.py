6eaadb92749b11dc1562e5a6d3ac52457dc15eb2
Brett <brettlev@umich.edu>
2020-12-25 18:25:31 -0800

    Implements feedforward propogation and random weight initilization

--- main.py
+++ main.py
@@ -3,13 +3,18 @@
 
 import MNIST
 import neural_network
+import numpy as np
 
 def main():
     print("Neural Network")
     
-    nn = neural_network.NeuralNetwork([100, 20, 10])
+    nn = neural_network.NeuralNetwork([3, 5, 2])
     
     data = MNIST.MNIST("Dataset/")
+
+    X = np.array([0.5, 1, 0.25]).T
+    out = nn.predict(X)
+    print(out)
 
 if __name__ == "__main__":
     main()
--- neural_network.py
+++ neural_network.py
@@ -7,12 +7,32 @@ class NeuralNetwork:
     
     # layers: array of nodes in each layer
     #         [10, 5, 10]
+    # [output x h_n] x ... x [h_n x h_1] x [h1 x input] x [input x 1]
     def __init__(self, layers):
-        self.W = []
+        self.W = [] # [next_layer x prev_layer x num_layers - 1]
+        
+        for i in range(len(layers) - 1):
+            self.W.append(np.random.rand(layers[i + 1], layers[i])) 
+
+        self.num_layers = len(layers)
+        assert(self.num_layers - 1 == len(self.W))
 
     def train(self, X, y):
         pass
 
     def predict(self, X):
-        pass
+        prev_layer = X
+        
+        for i in range(len(self.W)):
+            output = np.matmul(self.W[i], prev_layer)
+            output = self.activation(output)
+            prev_layer = output
 
+        return output    
+
+    # sigmoid activation function
+    def activation(self, val):
+        return 1 / (1 + np.exp(-val))
+
+    def derivative_activation(self, val):
+        return self.activation(val) * (1 - self.activation(val))
